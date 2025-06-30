a2a6c5e02d78be01c90a0330db7d8fdbde80cb63
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2025-06-30 12:08:39 +0200

    Update ensemble.py

--- .github/workflows/ensemble.py
+++ .github/workflows/ensemble.py
@@ -12,11 +12,45 @@ on:
     
 jobs:
   
-  ensemble_job:
+  verify_ensemble_job:
     if: ${{ github.event.workflow_run.conclusion == 'success' && github.repository_owner == 'Testing-Forecast-Actions' }}
     runs-on: ubuntu-latest
+    outputs: 
+      baseline_exists: ${{ steps.check_baseline.outputs.bl_exists == 'exists' }}
 
     steps:
-    
-    - name: test only step
-      run: ehco "Gira ensemble"
+      
+    # Checkout the python tools repo
+    # -------------------------------------------      
+    - name: checkout python tools repo
+      uses: actions/checkout@v3
+      with:
+        token: ${{ secrets.GITHUB_TOKEN }}
+        repository: 'Testing-Forecast-Actions/Testing-Tools'
+        ref: 'main'
+        path: './tools/'
+
+    # Checkout the data repository
+    # -------------------------------------------      
+    - name: checkout data repo
+      uses: actions/checkout@v3
+      with:
+        token: ${{ secrets.GITHUB_TOKEN }}
+        repository: 'Testing-Forecast-Actions/RespiCast-SyndromicIndicators'
+        ref: 'main'
+        path: './repo/'
+
+
+
+    # Run Pyton code
+    # -------------------------
+    - uses: actions/setup-python@v4
+      with:
+        python-version: '3.10' 
+
+    # Verify if baseline already exists
+    # this will write "exists" or "missing" in the out var baseline_exists
+    - name: check baseline exists
+      id: check_baseline
+      run: |
+        python ./tools/code/FH_current_baseline_exists.py --hub_path ./repo
